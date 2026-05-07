25ca83e1f9740f71e9a7c3adea16027c4979f90b
kennethsaavedrapac-web <kennethsaavedrapac@gmail.com>
2026-05-07 16:16:35 -0600

    Reinicio

--- test_script.js
+++
@@ -1,3 +0,0 @@
-const bd = require("./base-datos-salud.js");
-console.log("tos:", bd.buscarMultiplesMedicamentos("un medicamento para la tos").map(m => m.nombre_es));
-console.log("dolor:", bd.buscarMultiplesMedicamentos("un medicamento para el dolor").map(m => m.nombre_es));
